24eff719ae576e933bdf284338e1a40dff023c2b
SmileyBoy321 <ungurkristen@gmail.com>
2021-01-14 22:00:14 +0200

    html skeleton completed

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Python Project – Secure sensitive information by encoding messages.
+The objective of this project is to encode (or encrypt) and decode messages using a common key.
+This Python project will be built using Flask and base64 library.
+
+Additional notes for project improvement: Add more encode / decode support e.g AES, AES256 etc
+e.g = for example
+"""
