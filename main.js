41e287cf063a04cd13f8252389e4190f2021772a
SmileyBoy321 <ungurkristen@gmail.com>
2021-01-17 15:24:32 +0200

    js file refactored for more readability and easier to use

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-class EncodeMethods {
-    constructor(message) { this.message = message }
-    base64_encode() { return btoa(this.message); }
-    base64_decode() { return atob(this.message); }
+const actionTypeSelection = document.getElementById("types")
+const inputBox = document.getElementById("user_message_input");
+const outputBox = document.getElementById("result_of_message");
+
+const EncodeMethods = {
+    "Base64_encrypt": function (msg) {
+        return btoa(msg);
+    },
+    "Base64_decrypt": function (msg) {
+        return atob(msg);
+    }
 }
 
-const selectBox = document.getElementById("types")
-
-function detect_type() {
-    var encrypt_method = selectBox.options[selectBox.selectedIndex];
-    var optgroup = encrypt_method.parentNode;
-    var selectbox_dictionary = {
-        "Method": encrypt_method,
-        "Group": optgroup
-    };
-    // console.log("Types: " + encrypt_method.label + " " + optgroup.label);
-    return selectbox_dictionary;
+function get_action_function() {
+    var encrypt_method = actionTypeSelection.options[actionTypeSelection.selectedIndex];
+    return EncodeMethods[encrypt_method.label + "_" + encrypt_method.parentNode.id];
 }
 
-const encrypt_message = document.getElementById("user_message_input");
-const decrypt_message = document.getElementById("result_of_message");
+function update_output() {
+    outputBox.value = get_action_function()(inputBox.value);
+}
 
-selectBox.addEventListener("change", function() {
-    selectbox_detection = detect_type();
-    console.log(selectbox_detection.Group);
+actionTypeSelection.addEventListener("change", function () {
+    update_output();
 });
 
-
-encrypt_message.addEventListener("keyup", function() {
-    let encode_method = new EncodeMethods(encrypt_message.value);
-    console.log(encrypt_message.value);
-    decrypt_message.value = encode_method.base64_encode();
+inputBox.addEventListener("keyup", function () {
+    update_output();
 });
